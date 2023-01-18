087ae6fc18eb65d0e04d9f1dea126d6dcfe06b28
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-18 14:52:29 +0100

    Inizializzazione progetto

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*
+    STEPS:
+        Milestone 1
+            1. Creare un array di oggetti dove ogni oggetto rappresenta un post.
+            2. Popolare ogni oggetto con gli attributi che serviranno a stampare la card del post in questione
+                2A. - id del post, numero progressivo da 1 a n
+                    - nome autore,
+                    - foto autore,
+                    - data in formato americano (yyyy-mm-dd),
+                    - testo del post,
+                    - immagine (non tutti i post devono avere una immagine),
+                    - numero di likes.
+        Milestone 2
+            1. Creare la struttura html per stampare i post nemma pagina, usando come riferimento il file fornito
+        Milestone 3
+            1. Creare un secondo array dove salvare i post che hanno ricevuto un like
+            2. Creare un evento di click sul pulsante del like
+                2A. Il colore del testo del bottone cambia
+                2B. Il conteggio dei like relativo a quel post si incrementa
+                2C. Il post che ha ricevuto like viene pushato in un array dedicato
+*/
+console.log("Collegamento javascript ok");
